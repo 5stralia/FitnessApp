17b9f3cbdbb2a4d4bfd83c8c20a574138c514d9b
hoju <5stralia@gmail.com>
2021-06-02 20:10:03 +0900

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by 최호주 on 2021/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
