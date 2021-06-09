981b0917f3ce67f9d5a0cc637ded69e811f86155
hoju <5stralia@gmail.com>
2021-06-09 22:33:33 +0900

    Setting Goal UI
    기초 작업

---
+++ FitnessApp/SettingGoal/SettingGoalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingGoalViewController.swift
+//  FitnessApp
+//
+//  Created by 최호주 on 2021/06/09.
+//
+
+import UIKit
+
+class SettingGoalViewController: UIViewController {
+    @IBOutlet weak var weightGoalButton: UIButton!
+    @IBOutlet weak var steadyGoadButton: UIButton!
+    @IBOutlet weak var firstTitleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
